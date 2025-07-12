be8587a6bbc0cee3feb1c610dc90d7d556f3e3e8
KhushPatel2026 <khushspatel@gmail.com>
2025-07-12 15:21:53 +0530

    Home Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,8 +3,6 @@ import { useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Profile from "./Pages/Profile/Profile";
-import Logout from "./Components/Logout";
 import StackItLanding from "./Pages/Landing/Landing";
 import RichTextDemo from "./Pages/RichTextDemo/RichTextDemo";
 import Home from "./Pages/Home/Home";
@@ -13,37 +11,8 @@ import LoginPage from "./Pages/Authentication/Login/Login";
 import ProfilePage from "./Pages/Profile/Profile";
 
 function App() {
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  useEffect(() => {
-    const urlParams = new URLSearchParams(location.search);
-    const token = urlParams.get("token");
-
-    if (token) {
-      localStorage.setItem("token", token);
-      navigate("/profile", { replace: true });
-    }
-  }, [location, navigate]);
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    // Redirect to login if no token and trying to access protected routes
-    if (
-      !token &&
-      (location.pathname === "/profile" || location.pathname === "/")
-    ) {
-      navigate("/login", { replace: true });
-    }
-    // Redirect to profile if have token and on root
-    else if (token && location.pathname === "/") {
-      navigate("/profile", { replace: true });
-    }
-  }, [navigate, location.pathname]);
-
   return (
     <div>
-      <Logout />
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/ask-question" element={<AskQuestion />} />
